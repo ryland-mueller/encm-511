ad3f7ca557dc7bd6f6a896fa8b9c2b30dbd16c32
BogdanRB14 <bogdan.bacea@ucalgary.ca>
2025-10-28 07:22:33 -0700

    enjoy patrick

---
+++ lab4.X/ADC.h
@@ -0,0 +1,23 @@
+/* 
+ * File:   ADC.h
+ * Author: every
+ *
+ * Created on October 28, 2025, 7:22 AM
+ */
+
+#ifndef ADC_H
+#define	ADC_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+
+
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* ADC_H */
+
