3e88f9db95d11b3d254651930bbc9cb8905b327e
Optu-s <patrick.mitenko@ucalgary.ca>
2025-10-28 09:23:36 -0600

    do_ADC function

--- lab4.X/ADC.c
+++ lab4.X/ADC.c
@@ -1,1 +1,28 @@
+/*
+ * File:   uart.h
+ * Author: Ryland Mueller, Patrick Mitenko, Bogdan Bacea
+ *
+ * Created FOR ENCM 511
+ * PLEASE ADD DATE CREATED HERE: 2025-10-21
+ */
 
+#include "common.h"
+
+
+
+uint16_t do_adc(void)
+{
+    uint16_t value = 0;
+    
+    AD1CON1bits.SAMP = 1;       // start A/D conversion
+    delay_ms(1);
+    AD1CON1bits.SAMP = 0;
+    
+    while (!AD1CON1bits.DONE){} // wait for ADC read to finish
+        
+    value = ADC1BUF0;     //  set value to lower 16bits of adc buffer
+    
+    return value;
+    
+    
+}
--- lab4.X/ADC.h
+++ lab4.X/ADC.h
@@ -8,16 +8,6 @@
 #ifndef ADC_H
 #define	ADC_H
 
-#ifdef	__cplusplus
-extern "C" {
+uint16_t do_adc(void);
+       
 #endif
-
-
-
-
-#ifdef	__cplusplus
-}
-#endif
-
-#endif	/* ADC_H */
-
